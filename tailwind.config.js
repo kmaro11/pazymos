191fc189cb99709d12d632544b0400ea257f658a
RokasVilcinskasHaven <167087322+RokasVilcinskasHaven@users.noreply.github.com>
2025-06-23 11:51:26 +0300

    new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
   content: [
     "./resources/**/*.blade.php",
@@ -37,6 +39,9 @@ export default {
         'large-card': '0px 4px 40px 0px rgba(181, 178, 172, 0.16)',
         'soft': '0px 10px 30px 0px rgba(0, 0, 0, 0.06)',
         'circle': '0px 4px 8px 0px rgba(66, 71, 76, 0.06), 0px 0px 1px 0px rgba(66, 71, 76, 0.48)',
+      },
+      fontFamily: {
+        sans: ['Helvetica Neue', ...defaultTheme.fontFamily.sans],
       },
     },
   },
